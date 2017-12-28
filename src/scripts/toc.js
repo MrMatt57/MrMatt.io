38d43014130e603862e78bb84206e28498e6a28a
Matt Walker <matt@mrmatt.io>
2017-12-28 17:40:37 -0500

    toc refactor

--- src/scripts/toc.js
+++ src/scripts/toc.js
@@ -1,34 +1,40 @@
 function TableOfContents() {
-    if(document.querySelectorAll('#toc').length == 0){
-        return;
-    }
     var toc = "";
     var level = 2;
-    var container = document.querySelector('#articlebody');
-    var output = '#toc-content';
-    container.innerHTML = container.innerHTML.replace(
-        /<h([\d])(.*)>([^<]+)<\/h([\d])>/gi,
-        function(str, openLevel, id, titleText, closeLevel) {
-            if (openLevel != closeLevel) {
-                return str;
-            }
-            if (openLevel > level) {
-                toc += (new Array(openLevel - level + 1)).join('<ul>');
-            } else if (openLevel < level) {
-                toc += (new Array(level - openLevel + 1)).join('</li></ul>');
-            } else {
-                toc += (new Array(level + 1)).join('</li>');
-            } 
-            level = parseInt(openLevel);
-            var anchor = titleText.replace(/ /g, "_");
-            toc += '<li><a href="#' + anchor + '">' + titleText +
-                '</a>';
-            return '<h' + openLevel + '><a href="#' + anchor + '" id="' + anchor + '">' +
-                titleText + '</a></h' + closeLevel + '>';
+
+    var parseMatch = function(str, openLevel, id, titleText, closeLevel) {
+        if (openLevel != closeLevel) {
+            return str;
         }
-    );
-    if (level) {
-        toc += (new Array(level + 1)).join('</ul>');
+        if (openLevel > level) {
+            toc += (new Array(openLevel - level + 1)).join('<ul>');
+        } else if (openLevel < level) {
+            toc += (new Array(level - openLevel + 1)).join('</li></ul>');
+        } else {
+            toc += (new Array(level + 1)).join('</li>');
+        } 
+        level = parseInt(openLevel);
+        var anchor = titleText.replace(/ /g, "_");
+        toc += '<li><a href="#' + anchor + '">' + titleText +'</a>';
+        return '<h' + openLevel + '><a href="#' + anchor + '" id="' + anchor + '">' +
+            titleText + '</a></h' + closeLevel + '>';
     }
-    document.querySelector(output).innerHTML += toc;
+
+    var initToc = function() {
+        var container = document.querySelector('#articlebody');
+        var output = '#toc-content';
+        container.innerHTML = container.innerHTML.replace(
+            /<h([\d])(.*)>([^<]+)<\/h([\d])>/gi, function(str, openLevel, id, titleText, closeLevel) {
+                parseMatch(str, openLevel, id, titleText, closeLevel);
+            });
+    
+        if (level) {
+            toc += (new Array(level + 1)).join('</ul>');
+        }
+        document.querySelector(output).innerHTML += toc;
+    }
+
+    if(document.querySelectorAll('#toc').length > 0){
+        initToc();
+    }
 };
